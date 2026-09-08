b180d038d63a03336a7561d31f8209acb85959fb
ashimbuilds <ashim.codes@gmail.com>
2026-09-08 10:55:57 +0545

    feat: add PWA support via vite-plugin-pwa and initialize store configuration file

--- web/dev-dist/sw.js
+++ web/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-290dd570'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ohsjha8cpq"
+    "revision": "0.1bu7tqcgcpg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
